dc7173374a32a548e55247ecb79aa76e8d7b87ab
AustinLegenda <austin@legenda.co>
2025-03-08 21:48:36 -0500

    commit

--- wp-content/plugins/easy-invoice/templates/parts/before-items-1.php
+++ wp-content/plugins/easy-invoice/templates/parts/before-items-1.php
@@ -4,7 +4,13 @@ if (!defined('ABSPATH')) {
 }
 
 global $ei_invoice;
+
+// Ensure $ei_invoice is not null before calling methods
+if (!isset($ei_invoice) || !is_object($ei_invoice)) {
+    return; // Stop execution if $ei_invoice is not available
+}
 ?>
+
 <div class="lei-before-items-one | lei-section">
 	<div class="ei-title">
 		<?php if ($ei_invoice->get_deposit_amount() > 0) { ?>
