82ef77d902982bf57c00de5799ac0fdba8e1c90d
AustinLegenda <austin@legenda.co>
2025-03-09 08:56:46 -0400

    commit

--- wp-content/plugins/easy-invoice/includes/Admin/FieldItems/Editor.php
+++ wp-content/plugins/easy-invoice/includes/Admin/FieldItems/Editor.php
@@ -10,18 +10,20 @@ class Editor
 
 		$class = $field['class'] ?? '';
 
-		?>
+?>
 		<div class="matrixaddons-fieldset">
 			<?php
 
 			$settings = array(
-					'textarea_name' => $field_name,
-					'tinymce' => array(
-							'toolbar1' => 'bold,italic,underline, blockquote, strikethrough, bullist, numlist, separator,alignleft,aligncenter,alignright,separator,link,unlink,undo,redo'
-					),
-					'editor_height' => 150, // In pixels, takes precedence and has no default value
-
+				'textarea_name' => $field_name,
+				'tinymce' => array(
+					'toolbar1' => 'formatselect, bold,italic,underline, blockquote, strikethrough, bullist, numlist, alignleft, aligncenter, alignright, link, unlink, undo, redo, hr',
+					'toolbar2' => 'h1,h2,h3,h4,h5,h6'
+				),
+				'quicktags' => true, // Enables quicktags in text mode
+				'editor_height' => 150, // Height in pixels
 			);
+
 			$editor_id = $field_name . '_id';
 
 			$value = $value == null ? "" : $value;
@@ -29,37 +31,38 @@ class Editor
 			wp_editor($value, $editor_id, $settings);
 			?>
 		</div>
-		<?php
+<?php
 	}
 
 	public static function sanitize($field, $raw_value, $field_id)
 	{
 		$allowed_html = $field['allowed_html'] ?? array(
-						'p' => array(
-								'style' => array()
-						),
-						'a' => array('href' => array(), 'target' => array(), 'rel' => array()),
-						'br' => array(),
-						'b' => array(),
-						'strong' => array(),
-						'em' => array(),
-						'i' => array(),
-						'u' => array(),
-						'blockquote' => array(),
-						'del' => array(),
-						'ins' => array(),
-						'img' => array(
-								'src' => array(),
-								'height' => array(),
-								'width' => array()
-						),
-						'ul' => array(),
-						'ol' => array(),
-						'li' => array(),
-						'code' => array(),
-						'span' => array('style' => array()
-						)
-				);
+			'p' => array(
+				'style' => array()
+			),
+			'a' => array('href' => array(), 'target' => array(), 'rel' => array()),
+			'br' => array(),
+			'b' => array(),
+			'strong' => array(),
+			'em' => array(),
+			'i' => array(),
+			'u' => array(),
+			'blockquote' => array(),
+			'del' => array(),
+			'ins' => array(),
+			'img' => array(
+				'src' => array(),
+				'height' => array(),
+				'width' => array()
+			),
+			'ul' => array(),
+			'ol' => array(),
+			'li' => array(),
+			'code' => array(),
+			'span' => array(
+				'style' => array()
+			)
+		);
 
 		return wp_kses($raw_value, $allowed_html);
 	}
